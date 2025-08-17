965af64977cb5496c4611276be93970c10057daa
Soulzys <lopez.leo@hotmail.fr>
2025-08-17 21:03:11 +0200

    Removes glext.h and wglext.h and load only what is necessary by ourselves

--- code/win32_soth.cpp
+++ code/win32_soth.cpp
@@ -1,7 +1,5 @@
 #include <windows.h>
 #include <gl/gl.h>
-#include "../include/glext.h"
-#include "../include/wglext.h"
 #include <stdint.h>
 
 typedef uint8_t  uint8  ;
@@ -11,40 +9,80 @@ typedef uint64_t uint64 ;
 
 static bool g_Running;
 
+// From wglext.h
+//
+#define WGL_DRAW_TO_WINDOW_ARB            0x2001
+#define WGL_ACCELERATION_ARB              0x2003
+#define WGL_SUPPORT_OPENGL_ARB            0x2010
+#define WGL_DOUBLE_BUFFER_ARB             0x2011
+#define WGL_PIXEL_TYPE_ARB                0x2013
+#define WGL_COLOR_BITS_ARB                0x2014
+#define WGL_ALPHA_BITS_ARB                0x201B
+#define WGL_DEPTH_BITS_ARB                0x2022
+#define WGL_STENCIL_BITS_ARB              0x2023
+#define WGL_FULL_ACCELERATION_ARB         0x2027
+#define WGL_TYPE_RGBA_ARB                 0x202B
+#define WGL_SAMPLE_BUFFERS_ARB            0x2041
+#define WGL_SAMPLES_ARB                   0x2042
+#define WGL_CONTEXT_MAJOR_VERSION_ARB     0x2091
+#define WGL_CONTEXT_MINOR_VERSION_ARB     0x2092
 
-#define WGL_CHOOSE_PIXEL_FORMAT_ARB(name) BOOL WINAPI name(HDC hdc, const int* piAttribIList, const FLOAT* pfAttribFList, UINT nMaxFormats, int* piFormat, UINT* nNumFormats)
-typedef WGL_CHOOSE_PIXEL_FORMAT_ARB(wgl_choose_pixel_format_arb);
-WGL_CHOOSE_PIXEL_FORMAT_ARB(wglChoosePixelFormatArbStub)
+// From glext.h
+//
+typedef BOOL (WINAPI*   WGL_CHOOSE_PIXEL_FORMAT_ARB   ) (HDC hdc, const int* piAttribIList, const FLOAT* pfAttribFList, UINT nMaxFormats, int* piFormats, UINT* nNumFormats);
+typedef HGLRC(WINAPI*   WGL_CREATE_CONTEXT_ATTRIBS_ARB) (HDC hDC, HGLRC hShareContext, const int* attribList);
+typedef void (APIENTRY* GL_GEN_VERTEX_ARRAYS          ) (GLsizei n, GLuint* arrays);
+typedef void (APIENTRY* GL_GEN_BUFFERS                ) (GLsizei n, GLuint* buffers);
+typedef void (APIENTRY* GL_BIND_VERTEX_ARRAY          ) (GLuint array);
+typedef void (APIENTRY* GL_BIND_BUFFER                ) (GLenum target, GLuint buffer);
+typedef void (APIENTRY* GL_BUFFER_DATA                ) (GLenum target, ptrdiff_t size, const void* data, GLenum usage);
+typedef void (APIENTRY* GL_VERTEX_ATTRIB_POINTER      ) (GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const void* pointer);
+typedef void (APIENTRY* GL_ENABLE_VERTEX_ATTRIB_ARRAY ) (GLuint index);
+
+static WGL_CHOOSE_PIXEL_FORMAT_ARB    wglChoosePixelFormatARB_    ;
+static WGL_CREATE_CONTEXT_ATTRIBS_ARB wglCreateContextAttribsARB_ ;
+static GL_GEN_VERTEX_ARRAYS           glGenVertexArrays_          ;
+static GL_GEN_BUFFERS                 glGenBuffers_               ;
+static GL_BIND_VERTEX_ARRAY           glBindVertexArray_          ;
+static GL_BIND_BUFFER                 glBindBuffer_               ;
+static GL_BUFFER_DATA                 glBufferData_               ;
+static GL_VERTEX_ATTRIB_POINTER       glVertexAttribPointer_      ;
+static GL_ENABLE_VERTEX_ATTRIB_ARRAY  glEnableVertexAttribArray_  ;
+
+
+
+
+static bool Win32_LoadOpenGLFunctions()
 {
-	return(0);
+	wglChoosePixelFormatARB_    = (WGL_CHOOSE_PIXEL_FORMAT_ARB   )(wglGetProcAddress("wglChoosePixelFormatARB"   ));
+	if (!wglChoosePixelFormatARB_)    return false;
+
+	wglCreateContextAttribsARB_ = (WGL_CREATE_CONTEXT_ATTRIBS_ARB)(wglGetProcAddress("wglCreateContextAttribsARB"));
+	if (!wglCreateContextAttribsARB_) return false;
+
+	glGenVertexArrays_          = (GL_GEN_VERTEX_ARRAYS          )(wglGetProcAddress("glGenVertexArrays"         ));
+	if (!glGenVertexArrays_)          return false;
+
+	glGenBuffers_               = (GL_GEN_BUFFERS                )(wglGetProcAddress("glGenBuffers"              ));
+	if (!glGenBuffers_)               return false;
+
+	glBindVertexArray_          = (GL_BIND_VERTEX_ARRAY          )(wglGetProcAddress("glBindVertexArray"         ));
+	if (!glBindVertexArray_)          return false;
+
+	glBindBuffer_               = (GL_BIND_BUFFER                )(wglGetProcAddress("glBindBuffer"              ));
+	if (!glBindBuffer_)               return false;
+
+	glBufferData_               = (GL_BUFFER_DATA                )(wglGetProcAddress("glBufferData"              ));
+	if (!glBufferData_)               return false;
+
+	glVertexAttribPointer_      = (GL_VERTEX_ATTRIB_POINTER      )(wglGetProcAddress("glVertexAttribPointer"     ));
+	if (!glVertexAttribPointer_)      return false;
+
+	glEnableVertexAttribArray_  = (GL_ENABLE_VERTEX_ATTRIB_ARRAY )(wglGetProcAddress("glEnableVertexAttribArray" ));
+	if (!glEnableVertexAttribArray_)  return false;
+
+	return true;
 }
-static wgl_choose_pixel_format_arb* wglChoosePixelFormatARB_ = wglChoosePixelFormatArbStub;
-#define wglChoosePixelFormatARB wglChoosePixelFormatARB_;
-
-
-typedef BOOL (WINAPI* PFNWGLCHOOSEPIXELFORMATARBPROC) (HDC hdc, const int* piAttribIList, const FLOAT* pfAttribFList, UINT nMaxFormats, int* piFormats, UINT* nNumFormats);
-
-
-PFNWGLCHOOSEPIXELFORMATARBPROC toto;
-
-
-
-
-
-
-/*static void Win32_LoadOpenGLFunctions()
-{
-	//wglChoosePixelFormatARB = wglGetProcAddress("wglChoosePixelFormatARB");
-	toto = (PFNWGLCHOOSEPIXELFORMATARBPROC)wglGetProcAddress("wglChoosePixelFormatARB");
-	if (!toto)
-	{
-		OutputDebugString("\n\nWe've loaded the OpenGL 4.0 function !\n\n");
-	}
-	else
-	{
-		OutputDebugString("\n\nWe've failed loading the OpenGL 4.0 function... :(\n\n");
-	}
-}*/
 
 // TOTHINK: returning WindowHandle via the function's return value rather than via a out parameter ? 
 static bool Win32_InitOpenGL_2(HINSTANCE Instance, WNDCLASS* Window, HWND& WindowHandle)
@@ -53,25 +91,26 @@ static bool Win32_InitOpenGL_2(HINSTANCE Instance, WNDCLASS* Window, HWND& Windo
 
 	HWND _SacrificialWindow = CreateWindowEx
 	(
-		0, 
-		"Core",
-		"Sacrificial Window", 
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT, 
-		CW_USEDEFAULT, 
-		CW_USEDEFAULT, 
-		nullptr, 
-		nullptr, 
-		Instance, 
-		nullptr
+		0                    , 
+		"Core"               ,
+		"Sacrificial Window" , 
+		WS_OVERLAPPEDWINDOW  ,
+		CW_USEDEFAULT        ,
+		CW_USEDEFAULT        , 
+		CW_USEDEFAULT        , 
+		CW_USEDEFAULT        , 
+		0                    , 
+		0                    , 
+		Instance             , 
+		0
 	);
 
 	HDC _SacrificialDC = GetDC(_SacrificialWindow); // Handle to Device Context
+
 	PIXELFORMATDESCRIPTOR _SacrificialPFD = {};
-	_SacrificialPFD.nSize = sizeof(_SacrificialPFD);
-	_SacrificialPFD.nVersion = 1;
-	_SacrificialPFD.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+	_SacrificialPFD.nSize      = sizeof(_SacrificialPFD);
+	_SacrificialPFD.nVersion   = 1;
+	_SacrificialPFD.dwFlags    = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
 	_SacrificialPFD.iPixelType = PFD_TYPE_RGBA;
 	_SacrificialPFD.cColorBits = 32;
 	_SacrificialPFD.cAlphaBits = 8;
@@ -87,87 +126,79 @@ static bool Win32_InitOpenGL_2(HINSTANCE Instance, WNDCLASS* Window, HWND& Windo
 			{
 				if (wglMakeCurrent(_SacrificialDC, _SacrificialRC))
 				{
-					PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB_test = nullptr;
-					wglChoosePixelFormatARB_test = reinterpret_cast<PFNWGLCHOOSEPIXELFORMATARBPROC>(wglGetProcAddress("wglChoosePixelFormatARB"));
-					if (!wglChoosePixelFormatARB_test)
+					if (Win32_LoadOpenGLFunctions())
 					{
-						OutputDebugString("\nUtterly failed to load function :(\n\n");
-						return false;
-					}
+						// The real window
+						WindowHandle = CreateWindowEx
+						(
+							0,
+							Window->lpszClassName,
+							"Soth",
+							WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+							CW_USEDEFAULT,
+							CW_USEDEFAULT,
+							CW_USEDEFAULT,
+							CW_USEDEFAULT,
+							0,
+							0,
+							Instance,
+							0
+						);
+						HDC _DeviceContext = GetDC(WindowHandle);
 
-					PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB_test = nullptr;
-					wglCreateContextAttribsARB_test = reinterpret_cast<PFNWGLCREATECONTEXTATTRIBSARBPROC>(wglGetProcAddress("wglCreateContextAttribsARB"));
-					if (!wglCreateContextAttribsARB_test)
-					{
-						OutputDebugString("\nUtterly failed to load function :(\n\n");
-						return false;
-					}
-
-
-					// The real window
-					WindowHandle = CreateWindowEx
-					(
-						0,
-						Window->lpszClassName,
-						"Soth",
-						WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-						CW_USEDEFAULT,
-						CW_USEDEFAULT,
-						CW_USEDEFAULT,
-						CW_USEDEFAULT,
-						0,
-						0,
-						Instance,
-						0
-					);
-					HDC _DeviceContext = GetDC(WindowHandle);
-
-					int _PixelAttribs[] =
-					{
-						WGL_DRAW_TO_WINDOW_ARB , GL_TRUE                   , 
-						WGL_SUPPORT_OPENGL_ARB , GL_TRUE                   , 
-						WGL_DOUBLE_BUFFER_ARB  , GL_TRUE                   , 
-						WGL_PIXEL_TYPE_ARB     , WGL_TYPE_RGBA_ARB         , 
-						WGL_ACCELERATION_ARB   , WGL_FULL_ACCELERATION_ARB , 
-						WGL_COLOR_BITS_ARB     , 32                        , 
-						WGL_ALPHA_BITS_ARB     , 8                         , 
-						WGL_DEPTH_BITS_ARB     , 24                        ,
-						WGL_STENCIL_BITS_ARB   , 8                         , 
-						WGL_SAMPLE_BUFFERS_ARB , GL_TRUE                   , 
-						WGL_SAMPLES_ARB        , 4                         , 
-						0
-					};
-
-					int _PixelFormatID;
-					UINT _NumFormats;
-					if (wglChoosePixelFormatARB_test(_DeviceContext, _PixelAttribs, nullptr, 1, &_PixelFormatID, &_NumFormats))
-					{
-						if (_NumFormats == 0) return false;
-
-						PIXELFORMATDESCRIPTOR _PFD = {};
-						DescribePixelFormat(_DeviceContext, _PixelFormatID, sizeof(_PFD), &_PFD);
-						SetPixelFormat(_DeviceContext, _PixelFormatID, &_PFD);
-
-						// Requesting OpenGL 4.0
-						int _ContextAttribs[] =
+						int _PixelAttribs[] =
 						{
-							WGL_CONTEXT_MAJOR_VERSION_ARB , 4 , 
-							WGL_CONTEXT_MINOR_VERSION_ARB , 0 , 
+							WGL_DRAW_TO_WINDOW_ARB , GL_TRUE                   ,
+							WGL_SUPPORT_OPENGL_ARB , GL_TRUE                   ,
+							WGL_DOUBLE_BUFFER_ARB  , GL_TRUE                   ,
+							WGL_PIXEL_TYPE_ARB     , WGL_TYPE_RGBA_ARB         ,
+							WGL_ACCELERATION_ARB   , WGL_FULL_ACCELERATION_ARB ,
+							WGL_COLOR_BITS_ARB     , 32                        ,
+							WGL_ALPHA_BITS_ARB     , 8                         ,
+							WGL_DEPTH_BITS_ARB     , 24                        ,
+							WGL_STENCIL_BITS_ARB   , 8                         ,
+							WGL_SAMPLE_BUFFERS_ARB , GL_TRUE                   ,
+							WGL_SAMPLES_ARB        , 4                         ,
 							0 // Null terminate the array
 						};
 
-						HGLRC _RenderingContext = wglCreateContextAttribsARB_test(_DeviceContext, 0, _ContextAttribs);
-						if (_RenderingContext)
+						int _PixelFormatID;
+						UINT _NumFormats;
+						if (wglChoosePixelFormatARB_(_DeviceContext, _PixelAttribs, nullptr, 1, &_PixelFormatID, &_NumFormats))
 						{
-							wglMakeCurrent(nullptr, nullptr);
-							wglDeleteContext(_SacrificialRC);
-							ReleaseDC(_SacrificialWindow, _SacrificialDC);
-							DestroyWindow(_SacrificialWindow);
+							if (_NumFormats == 0) return false;
 
-							if (wglMakeCurrent(_DeviceContext, _RenderingContext))
+							PIXELFORMATDESCRIPTOR _PFD = {};
+							DescribePixelFormat(_DeviceContext, _PixelFormatID, sizeof(_PFD), &_PFD);
+							SetPixelFormat(_DeviceContext, _PixelFormatID, &_PFD);
+
+							// Requesting OpenGL 4.0
+							// TOTHINK: perhaps cover v3.3 as a backup ?
+							int _ContextAttribs[] =
 							{
-								// TODO: load OpenGL functions
-								SetWindowText(WindowHandle, (LPCSTR)glGetString(GL_VERSION));
+								WGL_CONTEXT_MAJOR_VERSION_ARB , 4 ,
+								WGL_CONTEXT_MINOR_VERSION_ARB , 0 ,
+								0
+							};
+
+							HGLRC _RenderingContext = wglCreateContextAttribsARB_(_DeviceContext, 0, _ContextAttribs);
+							if (_RenderingContext)
+							{
+								wglMakeCurrent(nullptr, nullptr);
+								wglDeleteContext(_SacrificialRC);
+								ReleaseDC(_SacrificialWindow, _SacrificialDC);
+								DestroyWindow(_SacrificialWindow);
+
+								if (wglMakeCurrent(_DeviceContext, _RenderingContext))
+								{
+									// TODO: load OpenGL functions
+									SetWindowText(WindowHandle, (LPCSTR)glGetString(GL_VERSION));
+								}
+								else
+								{
+									// LOGGING
+									return false;
+								}
 							}
 							else
 							{
