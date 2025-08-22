af0f95b0f74a73d1f8a4f3db2eaf3d11e508be92
Soulzys <lopez.leo@hotmail.fr>
2025-08-23 00:03:52 +0200

    Adds basic vector operations

--- code/utils.cpp
+++ code/utils.cpp
@@ -1,4 +1,5 @@
 #include "utils.h"
+#include <math.h>
 
 Vec3::Vec3()
 {
@@ -19,4 +20,57 @@ Vec3::Vec3(real32 ParamX, real32 ParamY, real32 ParamZ)
 	X = ParamX;
 	Y = ParamY;
 	Z = ParamZ;
+}
+
+real32 Vec3::Length() const
+{
+	return (real32)sqrt((X * X) + (Y * Y) + (Z * Z));
+}
+
+Vec3& Vec3::Normalize()
+{
+	float _Length = Length();
+	if (_Length > 0)
+	{
+		float _InvLength = 1 / _Length;
+		X *= _InvLength;
+		Y *= _InvLength;
+		Z *= _InvLength;
+	}
+
+	return *this;
+}
+
+real32 Vec3::Dot(const Vec3& V) const
+{
+	return (X * V.X) + (Y * V.Y) + (Z * V.Z);
+}
+
+Vec3 Vec3::Cross(const Vec3& V) const
+{
+	return Vec3(
+			       (Y * V.Z) - (Z * V.Y), 
+				   (Z * V.X) - (X * V.Z), 
+				   (X * V.Y) - (Y * V.X)
+			   );
+}
+
+Vec3 Vec3::operator+(const Vec3& V) const
+{
+	return Vec3(X + V.X, Y + V.Y, Z + V.Z);
+}
+
+Vec3 Vec3::operator-(const Vec3& V) const
+{
+	return Vec3(X - V.X, Y - V.Y, Z - V.Z);
+}
+
+Vec3 Vec3::operator*(const Vec3& V) const
+{
+	return Vec3(X * V.X, Y * V.Y, Z * V.Z);
+}
+
+Vec3 Vec3::operator/(const Vec3& V) const
+{
+	return Vec3(X / V.X, Y / V.Y, Z / V.Z);
 }
--- code/utils.h
+++ code/utils.h
@@ -6,6 +6,16 @@
 		Vec3(const Vec3& V);
 		Vec3(real32 ParamX, real32 ParamY, real32 ParamZ);
 
+		real32 Length   () const;
+		Vec3&  Normalize();
+		real32 Dot      (const Vec3& V) const;
+		Vec3   Cross    (const Vec3& V) const;
+
+		Vec3 operator+(const Vec3& V) const;
+		Vec3 operator-(const Vec3& V) const;
+		Vec3 operator*(const Vec3& V) const;
+		Vec3 operator/(const Vec3& V) const;
+
 		real32 X, Y, Z;
 	};
 //}
